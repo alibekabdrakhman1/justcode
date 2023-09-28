5503c094b5711f25c03e9f78ffd23ebf73da80e3
Alibek <alibekabdrakhman0@gmail.com>
2023-09-28 22:31:23 +0600

    fourth lecture first task

--- lecture4/first_task/example2/main.go
+++ lecture4/first_task/example2/main.go
@@ -1,1 +1,34 @@
-package example1
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var mutex sync.Mutex
+
+func print(str string) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	fmt.Println(str)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		print("JustCode1")
+	}()
+	go func() {
+		defer wg.Done()
+		print("JustCode2")
+	}()
+	go func() {
+		defer wg.Done()
+		print("JustCode3")
+	}()
+	wg.Wait()
+	fmt.Println("Done!!!")
+}
